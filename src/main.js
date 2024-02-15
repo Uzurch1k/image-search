34e5b5c996f1480f778efa8fd3de12fe7be64ccc
Uzurch1k <galcenkovlad7@gmail.com>
2024-02-15 19:10:18 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // ===============================================================
 
+import axios from 'axios';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
-import axios from 'axios';
 
 // ===============================================================
 
